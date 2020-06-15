d1355c44eabdb61ad332b24feb4aa3dec0ff8891
rizal690 <rizalhendri935@outlook.com>
2020-06-15 12:13:47 +0700

    Props pada Reactjs

--- src/Component/Fungsional/Home.jsx
+++ src/Component/Fungsional/Home.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import Blog from './Blog';
 
 const Home = () => {
-    return <div><h1>Ini adalah Home</h1></div>
+    return <div>
+    <Blog
+        tanggal="02 juni 2020"
+        judul="Teknologi Blockchain"
+        summary="Generate Lorem Ipsum placeholder text for use in your graphic."
+    />
+    <Blog
+        tanggal="03 juni 2020"
+        judul="Teknologi Internet of Things"
+        summary="Generate Lorem Ipsum placeholder text for use in your graphic."
+    />
+    <Blog
+        tanggal="04 juni 2020"
+        judul="Design Pattern Laravel"
+        summary="Generate Lorem Ipsum placeholder text for use in your graphic."
+    />
+
+    </div>
 }
 
 export default Home;
