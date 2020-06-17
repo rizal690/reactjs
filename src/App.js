e26cbb0198258a70422ef1ce6791853a1b91c66e
rizal690 <rizalhendri935@outlook.com>
2020-06-17 14:17:33 +0700

    Bootstrap pada Reactjs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import Parent from './Component/Class/Parent';
+import BootstrapComp from './Component/Class/BootstrapComp';
+
 // import logo from './logo.svg';
 // import Home from './Component/Fungsional/Home';
 // import Beranda from './Component/Class/Beranda';
@@ -8,7 +9,7 @@ import Parent from './Component/Class/Parent';
 function App() {
   return (
     <div className="App">
-      <Parent/>
+      <BootstrapComp/>
     </div>
   );
 }
