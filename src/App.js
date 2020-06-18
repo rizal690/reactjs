4339b90978c5dec92c8393f97642018f871c5026
rizal690 <rizalhendri935@outlook.com>
2020-06-18 22:29:47 +0700

    React Router dengan Passing Data

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import NavbarComp from './Component/Fungsional/NavbarComp';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import HomePage from './Component/Fungsional/HomePage';
 import AboutComp from './Component/Fungsional/AboutComp';
+import DetailComp from './Component/Fungsional/DetailComp';
 
 // import logo from './logo.svg';
 // import Home from './Component/Fungsional/Home';
@@ -17,6 +18,7 @@ const App = () => {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/about" component={AboutComp} />
+        <Route exact path="/detail/:id" component={DetailComp} />
       </Switch>
     </BrowserRouter>
 
